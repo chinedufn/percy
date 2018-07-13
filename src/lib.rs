18a688f43b37c2b693aaed40c5754c4a1044d51b
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-07-13 10:05:38 -0400

    Start trying to track parents

--- src/lib.rs
+++ src/lib.rs
@@ -62,12 +62,13 @@ enum TagType {
 #[macro_export]
 macro_rules! html {
     ($($remaining_html:tt)*) => {{
-        let mut active_node: Option<VirtualNode> = None;
+        let mut root_nodes: Vec<Rc<RefCell<VirtualNode>>> = vec![];
+        let mut active_node: Option<Rc<RefCell<VirtualNode>>> = None;
         let prev_tag_type: Option<TagType> = None;
+        {
+            recurse_html! { active_node root_nodes prev_tag_type $($remaining_html)* };
+        }
 
-        recurse_html! { active_node prev_tag_type $($remaining_html)* };
-
-        println!("{:#?}", active_node);
         active_node.unwrap()
     }};
 }
@@ -77,69 +78,77 @@ macro_rules! recurse_html {
     // The beginning of an element without any attributes.
     // For <div></div> this is
     // <div>
-    ($active_node:ident $prev_tag_type:ident < $start_tag:ident > $($remaining_html:tt)*) => {
+    ($active_node:ident $root_nodes:ident $prev_tag_type:ident < $start_tag:ident > $($remaining_html:tt)*) => {
         let mut new_node = VirtualNode::new(stringify!($start_tag));
+        let mut new_node = Rc::new(RefCell::new(new_node));
+
+        if $prev_tag_type == Some(TagType::Open) {
+            $active_node.as_mut().unwrap().borrow_mut().children.push(Rc::clone(&new_node));
+        } else if ($prev_tag_type == None) {
+            $root_nodes.push(Rc::clone(&new_node));
+        }
 
         $active_node = Some(new_node);
 
         let tag_type = Some(TagType::Open);
-        recurse_html! { $active_node tag_type $($remaining_html)* }
+        recurse_html! { $active_node $root_nodes tag_type $($remaining_html)* }
     };
 
     // The beginning of an element.
     // For <div id="10",> this is
     // <div
-    ($active_node:ident $prev_tag_type:ident < $start_tag:ident $($remaining_html:tt)*) => {
+    ($active_node:ident $root_nodes:ident $prev_tag_type:ident < $start_tag:ident $($remaining_html:tt)*) => {
         let mut new_node = VirtualNode::new(stringify!($start_tag));
+        let mut new_node = Rc::new(RefCell::new(new_node));
 
         $active_node = Some(new_node);
 
         let tag_type = Some(TagType::Open);
-        recurse_html! { $active_node tag_type $($remaining_html)* }
+        recurse_html! { $active_node $root_nodes tag_type $($remaining_html)* }
     };
 
     // The end of an opening tag.
     // For <div id="10",> this is:
     //  >
-    ($active_node:ident $prev_tag_type:ident > $($remaining_html:tt)*) => {
-        recurse_html! { $active_node $prev_tag_type $($remaining_html)* }
+    ($active_node:ident $root_nodes:ident $prev_tag_type:ident > $($remaining_html:tt)*) => {
+        recurse_html! { $active_node $root_nodes $prev_tag_type $($remaining_html)* }
     };
 
     // A property
     // For <div id="10",> this is:
     // id = "10",
-    ($active_node:ident $prev_tag_type:ident $prop_name:tt = $prop_value:expr, $($remaining_html:tt)*) => {
-        $active_node.as_mut().unwrap().props.insert(
+    ($active_node:ident $root_nodes:ident $prev_tag_type:ident $prop_name:tt = $prop_value:expr, $($remaining_html:tt)*) => {
+        $active_node.as_mut().unwrap().borrow_mut().props.insert(
             stringify!($prop_name).to_string(),
             $prop_value.to_string()
         );
 
-        recurse_html! { $active_node $prev_tag_type $($remaining_html)* }
+        recurse_html! { $active_node $root_nodes $prev_tag_type $($remaining_html)* }
     };
 
     // An event
     // for <div $onclick=|| { do.something(); },></div> ths is:
     //   $onclick=|| { do.something() }
-    ($active_node:ident $prev_tag_type:ident ! $event_name:tt = $callback:expr, $($remaining_html:tt)*) => {
-        $active_node.as_mut().unwrap().events.0.insert(
+    ($active_node:ident $root_nodes:ident $prev_tag_type:ident ! $event_name:tt = $callback:expr, $($remaining_html:tt)*) => {
+        $active_node.as_mut().unwrap().borrow_mut().events.0.insert(
             stringify!($event_name).to_string(),
             Box::new($callback)
         );
 
-        recurse_html! { $active_node $prev_tag_type $($remaining_html)* }
+        recurse_html! { $active_node $root_nodes $prev_tag_type $($remaining_html)* }
     };
 
 
     // A closing tag for some associated opening tag name
     // For <div id="10",></div> this is:
     // </div>
-    ($active_node:ident $prev_tag_type:ident < / $end_tag:ident > $($remaining_html:tt)*) => {
+    ($active_node:ident $root_nodes:ident $prev_tag_type:ident < / $end_tag:ident > $($remaining_html:tt)*) => {
         let tag_type = Some(TagType::Close);
-        recurse_html! { $active_node tag_type $($remaining_html)* }
+        recurse_html! { $active_node $root_nodes tag_type $($remaining_html)* }
     };
 
     // No more HTML remaining. We're done!
-    ($active_node:ident $prev_tag_type:ident) => {
+    ($active_node:ident $root_nodes:ident $prev_tag_type:ident) => {
     };
 
     // TODO: README explains that props must end with commas
@@ -162,7 +171,7 @@ mod tests {
             ..VirtualNode::default()
         };
 
-        assert_eq!(node, expected_node);
+        assert_eq!(node, Rc::new(RefCell::new(expected_node)));
     }
 
     #[test]
@@ -179,7 +188,7 @@ mod tests {
             ..VirtualNode::default()
         };
 
-        assert_eq!(node, expected_node);
+        assert_eq!(node, Rc::new(RefCell::new(expected_node)));
     }
 
     #[test]
@@ -197,7 +206,7 @@ mod tests {
 
         assert_eq!(test_struct.borrow().closure_ran, false);
 
-        node.events.0.get("onclick").unwrap()();
+        node.borrow().events.0.get("onclick").unwrap()();
 
         assert_eq!(test_struct.borrow().closure_ran, true);
     }
@@ -213,13 +222,17 @@ mod tests {
             tag: "span".to_string(),
             ..VirtualNode::default()
         }));
-        let children = vec![child];
+        let mut child_clone = Rc::clone(&child);
+        let mut children = vec![child];
+        // TODO: Add parent
 
-        let expected_node = VirtualNode {
+        let expected_node = Rc::new(RefCell::new(VirtualNode {
             tag: "div".to_string(),
             children,
             ..VirtualNode::default()
-        };
+        }));
+
+        child_clone.borrow_mut().parent = Some(Rc::clone(&expected_node));
 
         assert_eq!(node, expected_node);
     }
@@ -246,6 +259,6 @@ mod tests {
             ..VirtualNode::default()
         };
 
-        assert_eq!(node, expected_node);
+        assert_eq!(node, Rc::new(RefCell::new(expected_node)));
     }
 }
