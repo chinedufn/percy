933fea8d7af229c18911626d8f7fa1310c925975
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-07-15 22:13:17 -0400

    Add a second block with same text

--- examples/isomorphic/app/src/lib.rs
+++ examples/isomorphic/app/src/lib.rs
@@ -42,14 +42,16 @@ impl App {
     pub fn render(&self) -> VirtualNode {
         let state = Rc::clone(&self.state);
         let click_count = self.state.borrow().click_count();
-        let click_count = click_count.to_string();
 
-        let click_count = html! { <strong style="font-size: 30px",>{ click_count }</strong> };
+        let click_count = &click_count.to_string();
+
+        let click_component = html! { <strong style="font-size: 30px",>{ click_count }</strong> };
 
         html! {
         <div>
-          <span> { "The button has been clicked: " click_count  " times!"} </span>
+          <span> { "The button has been clicked: " click_component  " times!"} </span>
           <button !onclick=move|| { state.borrow_mut().msg(Msg::Click) },>{ "Click me!" }</button>
+          <div> { "In this time " click_count " rustaceans have been born." } </div>
         </div>
         }
     }
