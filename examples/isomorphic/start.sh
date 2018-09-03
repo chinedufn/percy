9a99e0dd06040b16ed243df619c287543c09e585
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-09-03 05:26:14 -0400

    Fix cd into root dir

--- examples/isomorphic/start.sh
+++ examples/isomorphic/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cd $(git rev-parse --show-toplevel)
+# cd to the root directory of this repository
+cd $(dirname $0)
+cd ../..
+
+exit 0
 
 ./examples/isomorphic/client/build-wasm.sh
 cargo +nightly run -p isomorphic-server
