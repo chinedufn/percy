63c6eab336d84b15abc1dbfd23bb8f3d7e95b611
qm3ster <qm3ster@gmail.com>
2018-09-16 23:26:02 +0300

    Use actix_web instead of naive server

--- examples/isomorphic/server/src/server.rs
+++ examples/isomorphic/server/src/server.rs
@@ -1,91 +1,38 @@
-use std::net::TcpListener;
-
-use std::io;
-use std::io::Read;
-use std::io::Write;
+extern crate actix_web;
+use self::actix_web::{fs, HttpRequest, HttpResponse, Responder};
 
 use isomorphic_app::App;
-use std::fs::File;
-use std::net::TcpStream;
-use std::prelude::v1::Vec;
-use std::string::String;
-use std::fs;
+
+const HTML_PLACEHOLDER: &str = "#HTML_INSERTED_HERE_BY_SERVER#";
+const STATE_PLACEHOLDER: &str = "#INITIAL_STATE_JSON#";
+
+fn index(req: &HttpRequest) -> impl Responder {
+    let app = App::new(
+        req.query()
+            .get("init")
+            .map(|string| string.parse().expect("bad param"))
+            .unwrap_or(1001),
+    );
+    let state = app.state.borrow();
+
+    let html = format!("{}", include_str!("./index.html"));
+    let html = html.replacen(HTML_PLACEHOLDER, &app.render().to_string(), 1);
+    let html = html.replacen(STATE_PLACEHOLDER, &state.to_json(), 1);
+
+    HttpResponse::Ok().content_type("text/html").body(html)
+}
 
 pub fn serve() {
-    let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
+    let server = actix_web::server::new(|| {
+        let app = actix_web::App::new();
+        let app = app.resource("/", |r| r.f(index));
+        let app = app.handler(
+            "/",
+            fs::StaticFiles::new("./examples/isomorphic/client/").unwrap(),
+        );
+        app
+    }).bind("0.0.0.0:7878")
+    .unwrap();
     println!("Listening on port 7878");
-
-    for stream in listener.incoming() {
-        println!("Incoming connection\n\n");
-
-        let stream = stream.unwrap();
-
-        handle_connection(stream);
-    }
-
-    fn handle_connection(mut stream: TcpStream) {
-        let mut buffer = [0; 512];
-        stream.read(&mut buffer).unwrap();
-
-        let request_pieces = String::from_utf8_lossy(&buffer);
-        let buffer_words = request_pieces.split("/").collect::<Vec<&str>>();
-
-        // Not sure what this request is but it's breaking stuff... so we ignore it..
-        // Worry about this later..
-        if buffer.len() < 5 || buffer_words.len() < 2 {
-            let response = format!("HTTP/1.1 404 NOT FOUND\r\n\r\n");
-            stream.write(response.as_bytes()).unwrap();
-            stream.flush().unwrap();
-
-            return;
-        }
-
-        let filename = buffer_words[1];
-
-        let filename = filename.split(" ").collect::<Vec<&str>>();
-        let filename = filename[0];
-
-        let filename = &format!("./examples/isomorphic/client/{}", filename);
-
-        let get_home = b"GET / HTTP/1.1\r\n";
-
-        if buffer.starts_with(get_home) {
-            stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
-            let app = App::new(1001);
-            let state = app.state.borrow();
-
-            let html = format!("{}", include_str!("./index.html"));
-            let html = html.replacen(
-                "#HTML_INSERTED_HERE_BY_SERVER#",
-                &app.render().to_string(),
-                1,
-            );
-            let html = html.replacen("#INITIAL_STATE_JSON#", &state.to_json(), 1);
-
-            stream.write(html.as_bytes()).unwrap();
-        } else {
-            let file_metadata = fs::metadata(filename);
-
-            if let Ok(file_metadata) = file_metadata {
-                let mut file = File::open(filename).unwrap();
-
-                stream.write(b"HTTP/1.1 200 OK").unwrap();
-
-                let content_length = file_metadata.len();
-                let content_length = format!("\r\nContent-Length: {}", content_length);
-                stream.write(content_length.as_bytes()).unwrap();
-
-                if String::from_utf8_lossy(&buffer).contains(".module.wasm") {
-                    stream.write(b"\r\nContent-Type: application/wasm").unwrap();
-                }
-                stream.write(b"\r\n\r\n").unwrap();
-                io::copy(&mut file, &mut stream).expect("Couldn't pipe file");
-            } else {
-                stream
-                    .write(format!("HTTP/1.1 404 Not Found\r\n\r\n").as_bytes())
-                    .unwrap();
-            };
-        }
-        stream.flush().unwrap();
-    }
+    server.run();
 }
