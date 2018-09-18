8602dd32540d93716e585b91eb5692fb0b00a3af
qm3ster <qm3ster@gmail.com>
2018-09-18 17:45:52 +0300

    Explicitly add webpack default paths
    For documentation purposes

--- examples/isomorphic/client/webpack.config.js
+++ examples/isomorphic/client/webpack.config.js
@@ -1,7 +1,14 @@
+const path = require('path')
+
 const mode =
   process.env.NODE_ENV === 'production' ? 'production' : 'development'
 module.exports = {
   mode,
+  entry: './src', // webpack default
+  output: {
+    path: path.resolve(__dirname, 'dist'), // webpack default
+    filename: 'bundle.js' // webpack default
+  },
   devServer: {
     proxy: { '/': { target: 'http://localhost:7878' } }
   }
