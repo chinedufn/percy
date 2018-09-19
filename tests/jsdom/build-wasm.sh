3feb618e0436743e5008eef75d11a32be778994e
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-09-19 18:45:22 -0400

    Remove unnecessary backslash

--- tests/jsdom/build-wasm.sh
+++ tests/jsdom/build-wasm.sh
@@ -2,5 +2,5 @@
 
 cd $(git rev-parse --show-toplevel)
 
-cargo +nightly build -p jsdom-tests --target wasm32-unknown-unknown && \
+cargo +nightly build -p jsdom-tests --target wasm32-unknown-unknown &&
   wasm-bindgen --nodejs --no-typescript target/wasm32-unknown-unknown/debug/jsdom_tests.wasm --out-dir ./tests/jsdom
