74897f58f0519a3019ff84d87252366d55eacfd5
qm3ster <qm3ster@gmail.com>
2018-09-19 03:12:42 +0300

    Only watch relevant sources

--- examples/isomorphic/watch.sh
+++ examples/isomorphic/watch.sh
@@ -5,5 +5,5 @@ cd $(dirname $0)
 cd ./client
 
 ./build-wasm.watch.sh &
-systemfd --no-pid -s http::7878 -- cargo +nightly watch -w ../ -x 'run -p isomorphic-server' &
+systemfd --no-pid -s http::7878 -- cargo +nightly watch -w ../server -w ../app -x 'run -p isomorphic-server' &
 wait
