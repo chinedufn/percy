da5dbccda03fffb9cbcd705316517133142e5b27
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-10-19 04:37:25 -0400

    Fix up dist scripts

--- examples/isomorphic/client/build-wasm.prod.sh
+++ examples/isomorphic/client/build-wasm.prod.sh
@@ -3,8 +3,8 @@
 cd $(dirname $0)
 
 rm -rf dist/
-mkdir -p build/
+mkdir -p dist/
 
 cargo +nightly build -p isomorphic-client --release --target wasm32-unknown-unknown &&
-  wasm-bindgen --no-modules --no-typescript ../../../target/wasm32-unknown-unknown/release/isomorphic_client.wasm --out-dir ./build &&
+  wasm-bindgen --no-modules --no-typescript ../../../target/wasm32-unknown-unknown/release/isomorphic_client.wasm --out-dir ./dist &&
   NODE_ENV=production ../../../node_modules/webpack-cli/bin/cli.js
--- examples/isomorphic/client/webpack.config.js
+++ examples/isomorphic/client/webpack.config.js
@@ -3,11 +3,13 @@ const path = require('path')
 const mode =
   process.env.NODE_ENV === 'production' ? 'production' : 'development'
 
+const outputDir = mode === 'production' ? 'dist' : 'build'
+
 module.exports = {
   mode,
   entry: './src',
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, outputDir),
     filename: 'bundle.js'
   },
   devServer: {
