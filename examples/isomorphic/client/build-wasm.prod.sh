38efa0bd7c7a8a91647d7eee64d5ac29e2d7845d
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2019-01-18 19:22:51 -0500

    Try fixing assets in example

--- examples/isomorphic/client/build-wasm.prod.sh
+++ examples/isomorphic/client/build-wasm.prod.sh
@@ -5,6 +5,7 @@ cd $(dirname $0)
 rm -rf dist/
 mkdir -p dist/
 
+# TODO: Move to wasm-pack
 cargo +nightly build -p isomorphic-client --release --target wasm32-unknown-unknown &&
   wasm-bindgen --no-modules --no-typescript ../../../target/wasm32-unknown-unknown/release/isomorphic_client.wasm --out-dir ./dist &&
   NODE_ENV=production ../../../node_modules/webpack-cli/bin/cli.js
