eb0c6deb4747be9f62ae824333ac8610c0727f99
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2019-03-16 14:00:42 -0400

    Start working on html validation

--- crates/html-validation/src/lib.rs
+++ crates/html-validation/src/lib.rs
@@ -1,33 +1,45 @@
-//! Validation for html attributes and elements
+//! The html-validation crate provides method that can be used when validating html elements
+//! and attributes.
+//!
+//! The original goal of this crate was to be used as a dependency in procedural macros that
+//! validate html at compile time, but it is general purpose and can be used in other problem
+//! spaces.
+//!
+//! ## Optimistic Validation
+//!
+//! The html-validation crate is optimistic by nature.
+//!
+//! This means that as we develop the create we'll blacklist more and more things - but in general
+//! we default to not saying that something is invalid until we've specifically encoded that it is
+//! not allowed.
+//!
+//! This means that you'll see methods with names like `is_definitely_invalid_child` - hinting
+//! that we're telling you that we're certain that the relationship is not allowed.
+//!
+//! Over time we'll cover more and more cases and this should become a non issue, but at the
+//! beginning it will mean that our validation is less strict than it should really be.
+//!
+//! The reason behind this strategy is that it lets people get up and running from day one without
+//! needing to wait until our validation is perfect.
+//! A downside is that as we become more and more strict there might be situations where you have
+//! to go back and tweak your html if you had something that we are now calling invalid.
 
 #![deny(missing_docs)]
 
-use lazy_static::lazy_static;
-use std::collections::hash_set::HashSet;
+pub use self_closing::is_self_closing;
 
-// Used to uniquely identify elements that contain closures so that the DomUpdater can
-// look them up by their unique id.
-// When the DomUpdater sees that the element no longer exists it will drop all of it's
-// Rc'd Closures for those events.
-lazy_static! {
-    static ref SELF_CLOSING_TAGS: HashSet<&'static str> = [
-        "area", "base", "br", "col", "hr", "img", "input", "link", "meta", "param", "command",
-        "keygen", "source",
-    ]
-    .iter()
-    .cloned()
-    .collect();
+mod self_closing;
+mod child_validation;
+mod content_kind;
+
+/// Holds just enough information about every type of element in order to power our validation.
+///
+/// For example, in order to know whether or not an anchor tag is an "interactive content"
+/// (from the html spec) we need to know if it has an `href` attribute.
+enum ElementTag<'a> {
+    A(Anchor<'a>)
 }
 
-/// Whether or not this tag is self closing
-///
-/// ```
-/// use html_validation::is_self_closing;
-///
-/// assert_eq!(is_self_closing("br"), true);
-///
-/// assert_eq!(is_self_closing("div"), false);
-/// ```
-pub fn is_self_closing(tag: &str) -> bool {
-    SELF_CLOSING_TAGS.contains(tag)
+struct Anchor<'a> {
+    href: Option<&'a str>
 }
