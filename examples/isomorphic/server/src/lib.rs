330b9282485bb9e2582bb36a3187b85c0031f329
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2019-05-26 10:32:42 -0400

    Add rocket example

--- examples/isomorphic/server/src/lib.rs
+++ examples/isomorphic/server/src/lib.rs
@@ -1,1 +1,11 @@
-pub mod server;
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[cfg(feature = "with-rocket")]
+#[macro_use]
+#[cfg(feature = "with-rocket")]
+extern crate rocket;
+
+#[cfg(feature = "with-actix")]
+pub mod actix_server;
+#[cfg(feature = "with-rocket")]
+pub mod rocket_server;
