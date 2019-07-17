34b5f68a837419a2bd7650f3fc4710573d62eff7
ci-build <ci-build@circleci>
2019-07-17 15:25:11 +0000

    [skip ci] Updates

---
+++ api/implementors/core/marker/trait.Freeze.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["router_rs"] = [{text:"impl&lt;T&gt; Freeze for <a class=\"struct\" href=\"router_rs/prelude/struct.Provided.html\" title=\"struct router_rs::prelude::Provided\">Provided</a>&lt;T&gt;",synthetic:true,types:["router_rs::provided::Provided"]},{text:"impl Freeze for <a class=\"struct\" href=\"router_rs/prelude/struct.Route.html\" title=\"struct router_rs::prelude::Route\">Route</a>",synthetic:true,types:["router_rs::route::Route"]},{text:"impl Freeze for <a class=\"struct\" href=\"router_rs/prelude/struct.Router.html\" title=\"struct router_rs::prelude::Router\">Router</a>",synthetic:true,types:["router_rs::router::Router"]},];
+implementors["virtual_dom_rs"] = [{text:"impl Freeze for <a class=\"struct\" href=\"virtual_dom_rs/struct.DomUpdater.html\" title=\"struct virtual_dom_rs::DomUpdater\">DomUpdater</a>",synthetic:true,types:["virtual_dom_rs::dom_updater::DomUpdater"]},{text:"impl&lt;'a&gt; Freeze for <a class=\"enum\" href=\"virtual_dom_rs/enum.Patch.html\" title=\"enum virtual_dom_rs::Patch\">Patch</a>&lt;'a&gt;",synthetic:true,types:["virtual_dom_rs::patch::Patch"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
