c1413474abf01483e9229963f61d29c73d7ef647
ci-build <ci-build@circleci>
2020-03-02 07:23:19 +0000

    [skip ci] Updates

--- api/implementors/core/ops/deref/trait.Deref.js
+++ api/implementors/core/ops/deref/trait.Deref.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["router_rs"] = [{"text":"impl&lt;T&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"router_rs/prelude/struct.Provided.html\" title=\"struct router_rs::prelude::Provided\">Provided</a>&lt;T&gt;","synthetic":false,"types":["router_rs::provided::Provided"]}];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- api/implementors/router_rs/prelude/trait.RouteParam.js
+++ api/implementors/router_rs/prelude/trait.RouteParam.js
@@ -1,9 +1,3 @@
 (function() {var implementors = {};
 implementors["router_rs"] = [];
-
-            if (window.register_implementors) {
-                window.register_implementors(implementors);
-            } else {
-                window.pending_implementors = implementors;
-            }
-        })()
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
