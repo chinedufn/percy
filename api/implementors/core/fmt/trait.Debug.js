0b6eafa8554e0ae5eff7fc2497944883b89f865a
ci-build <ci-build@circleci>
2020-09-17 22:47:40 +0000

    [skip ci] Updates

--- api/implementors/core/fmt/trait.Debug.js
+++ api/implementors/core/fmt/trait.Debug.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {};
-implementors["router_rs"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"struct\" href=\"router_rs/prelude/struct.Route.html\" title=\"struct router_rs::prelude::Route\">Route</a>","synthetic":false,"types":["router_rs::route::Route"]}];
-implementors["virtual_dom_rs"] = [{"text":"impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"virtual_dom_rs/enum.Patch.html\" title=\"enum virtual_dom_rs::Patch\">Patch</a>&lt;'a&gt;","synthetic":false,"types":["virtual_dom_rs::patch::Patch"]}];
+implementors["router_rs"] = [{"text":"impl Debug for Route","synthetic":false,"types":[]}];
+implementors["virtual_dom_rs"] = [{"text":"impl&lt;'a&gt; Debug for Patch&lt;'a&gt;","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
