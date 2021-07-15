92ff2f0405edb1f0a2ea140a7459942880c82df9
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2021-07-14 20:17:06 -0400

    Add percy-state crate

---
+++ crates/percy-state/src/lib.rs
@@ -0,0 +1,49 @@
+//! Used to manage application state.
+
+#![deny(missing_docs)]
+
+use std::sync::{Arc, RwLock, RwLockReadGuard};
+
+/// Holds application state.
+///
+/// # Cloning
+///
+/// It can be useful to clone `AppStateWrapper`'s in order to pass state into event handler
+/// closures.
+///
+/// All clones will point to the same inner state.
+///
+/// Cloning an `AppStateWrapper` is a very cheap operation.
+#[derive(Clone)]
+pub struct AppStateWrapper<S: AppState>(Arc<RwLock<S>>);
+
+/// Application state.
+pub trait AppState {
+    /// Indicates that something has happened.
+    ///
+    /// ```
+    /// # use std::time::SystemTime;
+    /// #[allow(unused)]
+    /// enum MyMessageType {
+    ///     IncreaseClickCounter,
+    ///     SetLastPausedAt(SystemTime)
+    /// }
+    /// ```
+    type Message;
+
+    /// Send a message to the state object.
+    /// This will usually lead to a state update
+    fn msg(&mut self, message: Self::Message);
+}
+
+impl<S: AppState> AppStateWrapper<S> {
+    /// Acquire write access to the AppState then send a message.
+    pub fn msg(&mut self, msg: S::Message) {
+        self.0.write().unwrap().msg(msg);
+    }
+
+    /// Acquire read access to AppState.
+    pub fn read(&self) -> RwLockReadGuard<'_, S> {
+        self.0.read().unwrap()
+    }
+}
