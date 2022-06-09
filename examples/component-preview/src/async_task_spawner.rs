039c8f142a78c7e982abdf93062727fdad66659a
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2022-06-09 10:33:09 -0400

    Fix tests

--- examples/component-preview/src/async_task_spawner.rs
+++ examples/component-preview/src/async_task_spawner.rs
@@ -1,9 +1,12 @@
-use percy_preview_app::async_task_spawner::{AsyncFnToSpawn, AsyncTaskSpawner};
-
 pub struct WebAsyncTaskSpawner;
 
-impl AsyncTaskSpawner for WebAsyncTaskSpawner {
-    fn spawn(&self, task: AsyncFnToSpawn) {
-        wasm_bindgen_futures::spawn_local(task)
+#[cfg(feature = "preview")]
+mod impl_preview {
+    use percy_preview_app::async_task_spawner::{AsyncFnToSpawn, AsyncTaskSpawner};
+
+    impl AsyncTaskSpawner for super::WebAsyncTaskSpawner {
+        fn spawn(&self, task: AsyncFnToSpawn) {
+            wasm_bindgen_futures::spawn_local(task)
+        }
     }
 }
